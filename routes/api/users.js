0304c168bfb6608fa66d49bd379b4e9e1fc2c401
Prasoon Jha <prasoonjha69@gmail.com>
2020-06-10 02:16:01 +0530

    jwt implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -61,8 +63,20 @@ router.post(
       await user.save();
 
       //return jsonwebtoken
-
-      res.send("User registered!");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
