83da87b4e1a53c67db1820dd9d58e4e4adc960c4
Prasoon Jha <prasoonjha69@gmail.com>
2020-06-09 20:35:23 +0530

    connection to mongoURI and routes set up

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//connect to database
+connectDB();
+
 app.get("/", (req, res) => res.send("API running"));
+
+//Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
