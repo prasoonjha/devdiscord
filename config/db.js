3cf1093beb21534ffec69fd591bec2027baddc54
Prasoon Jha <prasoonjha69@gmail.com>
2020-06-10 14:17:42 +0530

    deprecations fixed, added get all profiles route

--- config/db.js
+++ config/db.js
@@ -8,6 +8,8 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
     });
     console.log("MongoDB connected...");
   } catch (err) {
