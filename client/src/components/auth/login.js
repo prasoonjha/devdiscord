0ab6093da5845445f51350191c3e378754fd2412
Prasoon Jha <prasoonjha69@gmail.com>
2020-06-12 19:08:52 +0530

    Signup/signin components set up

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Button from "../UIElements/button";
+import "./login.css";
 
 const Login = () => {
-  return <div>LOgIN Route</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    alert("success");
+  };
+
+  return (
+    <React.Fragment>
+      <div className="form-container">
+        <div className="form-title">
+          <h2 className="">Log in to your account.</h2>
+        </div>
+        <form className="form-body" action="">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+          <div className="button-container">
+            <Button onClick={handleSubmit}>Login</Button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Button from "../UIElements/button";
+import "./register.css";
 
 const Register = () => {
-  return <div>Register Route</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = formData;
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    alert("success");
+  };
+
+  return (
+    <React.Fragment>
+      <div className="form-container">
+        <div className="form-title">
+          <h2 className="">Sign Up!</h2>
+          <p>Create your account. It's free. Now and forever!</p>
+        </div>
+        <form className="form-body">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+          <div className="button-container">
+            <Button onClick={handleSubmit}>Submit</Button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Register;
