54252f8d9ccfb5cdad7b6bfb07cde2abfabb054a
Prasoon Jha <prasoonjha69@gmail.com>
2020-06-25 01:09:27 +0530

    create profile action and component fin

--- client/src/components/profile-forms/createProfile.jsx
+++ client/src/components/profile-forms/createProfile.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import { createProfile } from "../../actions/profile";
+
 import Button from "../UIElements/button";
 
-const CreateProfile = () => {
+import "./createProfile.css";
+
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: "",
     location: "",
@@ -41,6 +46,7 @@ const CreateProfile = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    createProfile(formData, history);
   };
 
   return (
@@ -50,8 +56,8 @@ const CreateProfile = () => {
         <p>Let's get some information to make yourprofile stand out!</p>
       </div>
       <form className="form-body">
-        <select name="status">
-          <option value="0">* Select professional status</option>
+        <select name="status" value={status} onChange={handleChange}>
+          <option value="0">Select professional status</option>
           <option value="Developer">Developer</option>
           <option value="Junior developer">Junior developer</option>
           <option value="Senior developer">Senior developer</option>
@@ -61,14 +67,89 @@ const CreateProfile = () => {
           <option value="Intern">Intern</option>
           <option value="Other">Other</option>
         </select>
-        {/* <input
+        <input
           type="text"
-          placeholder="Name"
-          name="name"
-          value={name}
+          placeholder="Company"
+          name="company"
+          value={company}
           onChange={handleChange}
           required
-        /> */}
+        />
+        <input
+          type="text"
+          placeholder="Location"
+          name="location"
+          value={location}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Website"
+          name="website"
+          value={website}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Skills"
+          name="skills"
+          value={skills}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Github username"
+          name="githubusername"
+          value={githubusername}
+          onChange={handleChange}
+          required
+        />
+        <textarea
+          type="text"
+          placeholder="A short bio of yourself"
+          name="bio"
+          value={bio}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="LinkedIn URL"
+          name="linkedin"
+          value={linkedin}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Youtube URL"
+          name="youtube"
+          value={youtube}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Twitter URL"
+          name="twitter"
+          value={twitter}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Instagram URL"
+          name="instagram"
+          value={instagram}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Facebook URL"
+          name="facebook"
+          value={facebook}
+          onChange={handleChange}
+        />
         <div className="button-container">
           <Button onClick={handleSubmit}>Submit</Button>
         </div>
@@ -77,6 +158,8 @@ const CreateProfile = () => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+};
 
-export default CreateProfile;
+export default connect(null, { createProfile })(withRouter(CreateProfile));
